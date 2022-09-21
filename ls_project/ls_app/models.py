f39a407e8efbf2371e7f32904ed28da3ec58a7ab
ChikangaTakudzwa <takudzwachikanga@outlook.com>
2022-09-21 19:44:16 +0000

    Mod models upload to path

--- ls_project/ls_app/models.py
+++ ls_project/ls_app/models.py
@@ -9,8 +9,8 @@ class beats(models.Model):
     """Model of beats."""
     beat_name = models.CharField(max_length=50, help_text="Enter beat name")
     producer = models.CharField(max_length=50, help_text="Enter producer name")
-    artwork = models.ImageField(upload_to ='home/TakudzwaChikanga/mbidho/ls_project/ls_app/media',help_text="Upload artwork")
-    the_file = models.FileField(upload_to ='home/TakudzwaChikanga/mbidho/ls_project/ls_app/media',help_text="Upload beat")
+    artwork = models.ImageField(upload_to ='ls_app/media',help_text="Upload artwork")
+    the_file = models.FileField(upload_to ='ls_app/media',help_text="Upload beat")
     genre = models.CharField(max_length=20, choices=beat_genres,default=1, help_text="Enter beat genre")
     beat_type = models.CharField(max_length=20, choices=beat_type, default=1, help_text="Enter beat Type")
     # Buy = models.BooleanField(verbose_name ='Buy', default=False)

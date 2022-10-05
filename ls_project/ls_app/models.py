2570b32fc3a2b53d488e6e79729553fc507168ac
Replit user <>
2022-10-05 11:33:44 +0000

    fixed flake problems

--- ls_project/ls_app/models.py
+++ ls_project/ls_app/models.py
@@ -11,19 +11,19 @@ class beats(models.Model):
     beat_name = models.CharField(max_length=50, help_text="Enter beat name")
     producer = models.CharField(max_length=50, help_text="Enter producer name")
     artwork = models.ImageField(upload_to='ls_app/media',
-                              help_text="Upload artwork")
+                                help_text="Upload artwork")
     the_file = models.FileField(upload_to='ls_app/media',
-                              help_text="Upload beat")
+                                help_text="Upload beat")
     genre = models.CharField(max_length=20,
-                           choices=beat_genres,
-                           default=1,
-                           help_text="Enter beat genre")
+                             choices=beat_genres,
+                             default=1,
+                             help_text="Enter beat genre")
     beat_type = models.CharField(max_length=20,
-                               choices=beat_type,
-                               default=1,
-                               help_text="Enter beat Type")
+                                 choices=beat_type,
+                                 default=1,
+                                 help_text="Enter beat Type")
     rel_date = models.DateField(help_text="Enter beat release date")
 
-  def __str__(self):
-    """String for representing the Model object."""
-    return self.beat_name
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.beat_name

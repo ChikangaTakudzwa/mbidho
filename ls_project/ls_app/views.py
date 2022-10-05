b291ba870b9e1610b1918dee9306a7f1fc2ade20
Replit user <>
2022-10-05 11:29:58 +0000

    fixed flake problems

--- ls_project/ls_app/views.py
+++ ls_project/ls_app/views.py
@@ -8,16 +8,15 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    all_beats = beats.objects.all().order_by('-rel_date').values()
-    num_of_beats = beats.objects.all().count()
-    paginator = Paginator(all_beats, 9)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        'title': 'Live Sounds Records - Get Beats',
-        'all_beats': all_beats,
-        'num_of_beats': num_of_beats,
-        'page_obj': page_obj,
-    }
-    return render(request, 'index.html', context)
-  
+  all_beats = beats.objects.all().order_by('-rel_date').values()
+  num_of_beats = beats.objects.all().count()
+  paginator = Paginator(all_beats, 9)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
+  context = {
+    'title': 'Live Sounds Records - Get Beats',
+    'all_beats': all_beats,
+    'num_of_beats': num_of_beats,
+    'page_obj': page_obj,
+  }
+  return render(request, 'index.html', context)

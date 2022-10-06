5c85be3e3fcd6e95df50ffe9c19906f9e54c0eb8
ChikangaTakudzwa <takudzwachikanga@outlook.com>
2022-10-06 19:51:09 +0000

    Fixed pylint Issues4

--- ls_project/ls_app/apps.py
+++ ls_project/ls_app/apps.py
@@ -1,9 +1,10 @@
 """ App config """
 from django.apps import AppConfig
 
+
 class LsAppConfig(AppConfig):
 
     """ My App Config """
-    
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ls_app'
